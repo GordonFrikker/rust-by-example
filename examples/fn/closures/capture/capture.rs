52d0437c1136f2332daa8eb3091e1b3334e086ba
Ilya Bogdanov <fumlead@gmail.com>
2017-03-18 12:06:18 +0300

    Capture translated

--- examples/fn/closures/capture/capture.rs
+++ examples/fn/closures/capture/capture.rs
@@ -8,9 +8,15 @@ fn main() {
     // variable. It will remain borrowed until `print` goes out of
     // scope. `println!` only requires `by reference` so it doesn't
     // impose anything more restrictive.
+    // Замыкание для вывода `color`, которое немедленно заимствует (`&`)
+    // `color` и сохраняет заимствование и замыкание в переменной `print`.
+    // `color` будет оставаться заимствованным до выхода `print` из области
+    // видимости. `println!` требует только ссылку, поэтому он не накладывает
+    // никаких ограничений.
     let print = || println!("`color`: {}", color);
 
     // Call the closure using the borrow.
+    // Вызываем замыкание, используя заимствование.
     print();
     print();
 
@@ -23,32 +29,47 @@ fn main() {
     // A `mut` is required on `inc` because a `&mut` is stored inside.
     // Thus, calling the closure mutates the closure which requires
     // a `mut`.
+    // Замыкание для увеличения `count` может принимать как `&mut count`
+    // так и `count`, но `&mut count` менее ограничивающее, так что оно
+    // выбирает это. Немедленно заимствует `count`.
+    //
+    // `mut` требуется для `inc`, потому что `&mut` значение хранится внутри.
+    // Таким образом, вызов замыкания изменяет его, что требует `mut`
     let mut inc = || {
         count += 1;
         println!("`count`: {}", count);
     };
 
     // Call the closure.
+    // Вызываем замыкание.
     inc();
     inc();
 
     //let reborrow = &mut count;
     // ^ TODO: try uncommenting this line.
+    // ^ TODO: попробуйте раскомментировать эту строку.
 
     // A non-copy type.
+    // Тип без возможности копирования.
     let movable = Box::new(3);
 
     // `mem::drop` requires `T` so this must take by value. A copy type
     // would copy into the closure leaving the original untouched.
     // A non-copy must move and so `movable` immediately moves into
     // the closure.
+    // `mem::drop` требует `T`, так что захват производится по значению.
+    // Копируемый тип будет скопирован в замыкание, оставив оригинальное
+    // значение без изменения. Некопируемый тип должен быть перемещен и
+    // `movable` немедленно перемещается в замыкание.
     let consume = || {
         println!("`movable`: {:?}", movable);
         mem::drop(movable);
     };
 
     // `consume` consumes the variable so this can only be called once.
+    // `consume` поглощает переменную, так что оно может быть вызвано только раз.
     consume();
     //consume();
     // ^ TODO: Try uncommenting this line.
+    // ^ TODO: Попробуйте раскомментировать эту строку.
 }
--- examples/fn/closures/capture/input.md
+++ examples/fn/closures/capture/input.md
@@ -2,19 +2,25 @@ Closures are inherently flexible and will do what the functionality requires
 to make the closure work without annotation. This allows capturing to
 flexibly adapt to the use case, sometimes moving and sometimes borrowing.
 Closures can capture variables:
+Замыкания по своей сути достаточно гибкие и предоставляют возможность работы с
+замыканиями без дополнительного объяснения. Это позволяет адаптироваться в
+зависимости от конкретной необходимости, иногда перемещая значения, а иногда
+заимствуя их.
+Замыкания могут захватывать переменные:
 
-* by reference: `&T`
-* by mutable reference: `&mut T`
-* by value: `T`
+* по ссылке: `&T`
+* по изменяемой ссылке: `&mut T`
+* по значению: `T`
 
 They preferentially capture variables by reference and only go lower when
 required.
+Они преимущественно захватывают переменные по ссылке, если не указано обратное.
 
 {capture.play}
 
 ### Смотрите также:
 
-[`Box`][box] and [`std::mem::drop`][drop]
+[`Box`][box] и [`std::mem::drop`][drop]
 
 [box]: ../../std/box.html
 [drop]: http://doc.rust-lang.org/std/mem/fn.drop.html
